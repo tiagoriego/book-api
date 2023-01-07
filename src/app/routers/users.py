9f5426b3a0355d9e09a091fe8d4e3782e5041368
Tiago Riego <tiago.riego@yandeh.com.br>
2023-01-07 17:23:11 -0300

    Add new methods

--- src/app/routers/users.py
+++ src/app/routers/users.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, HTTPException, status, Depends, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from jose import jwt
 from utils import security
@@ -7,8 +7,9 @@ from services import user_service
 from config.variables import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 from models.token import Token
 from models.login import Login
-from models.user import User
+from models.user import User, UpdateUser, UpdatePassowrd
 from config.headers import get_current_user
+import re
 
 router = APIRouter()
 
@@ -75,3 +76,58 @@ async def get_me(current_user: User = Depends(get_current_user)):
         full_name=current_user.full_name
     )
     return user
+
+
+@router.patch("/users", response_model=User)
+async def update_user(user: UpdateUser, current_user: User = Depends(get_current_user)):
+    user_email = user_service.get_user_by_email(email=user.email)
+    if user_email:
+        if user_email.id != current_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Email cannot be used.")
+
+    is_valide_email = re.search(
+        "^[a-zA-Z0-9_\.]+@([a-zA-Z0-9]+\.)+[a-zA-Z0-9]{2,4}$", user.email)
+    if not is_valide_email:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email is not valid.")
+
+    result = user_service.update_partial_user(
+        id=current_user.id,
+        full_name=user.full_name,
+        email=user.email)
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Unable to update user.")
+
+    same_user = user_service.get_user_by_id(current_user.id)
+    return User(id=same_user.id,
+                full_name=same_user.full_name,
+                username=same_user.username,
+                email=same_user.email)
+
+
+@router.patch("/users/password", status_code=status.HTTP_204_NO_CONTENT)
+async def update_user_password(user: UpdatePassowrd, current_user: User = Depends(get_current_user)):
+
+    if user.old_password == user.new_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Old password it is the same new password.")
+
+    old_user_password = user_service.get_user_by_id(current_user.id)
+    if not old_user_password:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
+
+    if not security.verify_password(user.old_password, old_user_password.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Old password does not match.")
+
+    result = user_service.update_user_password(
+        id=current_user.id, hashed_password=security.get_password_hash(user.new_password))
+
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Unable to update user.")
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
